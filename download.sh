a58dfba6e40881828a1b896f9f84740f69dad8c4
t18nguye <khoa.van.nguyen@trustingsocial.com>
2021-10-17 20:49:07 +1100

    modified id field

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-gdown --fuzzy 'https://drive.google.com/file/d/182YXOZdP4sFMT5WceGLCQKTLAZa1GvTO/view?usp=sharing'
+gdown 'https://drive.google.com/u/2/uc?id=182YXOZdP4sFMT5WceGLCQKTLAZa1GvTO&export=download'
 unzip pretrained_models.zip
 rm pretrained_models.zip
